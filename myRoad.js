ee69e0b2fe1796860aeaf0a994b21be0d9df4bfe
ptitchkin <ptitchkin@gmail.com>
2017-04-23 16:43:49 -0400

    rename counter object to kb

--- myRoad.js
+++ myRoad.js
@@ -1,13 +1,13 @@
 /**
  * Created by ptitchkin on 4/23/2017.
  */
-var counter = (function () {
+var kb = (function () {
     var i = 0;
     return {
         setDiv: function (path) {
             //var node = $.getElementById(path);
             var node = $(path);//.append("<h1>hello I'll be here</h1>");
-            var svg = node.append("<svg style='position: absolute;width: 100%;height: 500px;border: 1px lightgray solid;'>" +
+            var svg = node.append("<svg style='position: absolute;width: 90%;height: 500px;border: 1px lightgray solid;'>" +
                 '<g>'+
                 '<circle cy="100" cx="200" r="30"/>' +
                 '<rect class="active" x="210" y="200" width="100" height="50" />' +
